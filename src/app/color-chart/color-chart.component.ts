f9011abf40555360b71165ef1c9fc97a79b97190
jp <jparker42@live.com>
2018-03-28 00:15:53 -0500

    color charts components - lighbox jpg files for display and thumbnails

--- src/app/color-chart/color-chart.component.ts
+++ src/app/color-chart/color-chart.component.ts
@@ -9,12 +9,17 @@ import {LightboxModule} from 'primeng/lightbox';
 })
 export class ColorChartComponent implements OnInit {
 
-  images: any[];
+  concretes: any[];
+  epoxies: any[];
 
-  constructor() { 
-    this.images = [];
-    this.images.push({source: 'assets/color-charts/epoxy-solid-cc.jpg', thumbnail: 'assets/color-charts/epoxy-solid-cc-thumb.jpg', title: 'Solid Epoxy'})
-    this.images.push({source: 'assets/color-charts/epoxy-quartz-cc.jpg', thumbnail: 'assets/color-charts/epoxy-quartz-cc-thumb.jpg', title: 'Quartz Epoxy'})
+  constructor() {
+    this.concretes = []; 
+    this.concretes.push({source: 'assets/color-charts/concrete-stain-cc.jpg', thumbnail: 'assets/color-charts/concrete-stain-cc-thumb.jpg', title: 'Concrete Stain'});
+
+    this.epoxies = [];
+    this.epoxies.push({source: 'assets/color-charts/epoxy-solid-cc.jpg', thumbnail: 'assets/color-charts/epoxy-solid-cc-thumb.jpg', title: 'Solid Epoxy'});
+    this.epoxies.push({source: 'assets/color-charts/epoxy-quartz-cc.jpg', thumbnail: 'assets/color-charts/epoxy-quartz-cc-thumb.jpg', title: 'Quartz Epoxy'});
+    this.epoxies.push({source: 'assets/color-charts/epoxy-metallic-cc.jpg', thumbnail: 'assets/color-charts/epoxy-metallic-cc-thumb.jpg', title: 'Metallic Epoxy'});
   }
 
   ngOnInit() {
