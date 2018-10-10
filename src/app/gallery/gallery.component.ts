9075acda7a447ce5ddfd70b542e2c6b9fb2e56bc
jp <jparker42@live.com>
2018-10-10 12:08:14 -0500

    new gallery pics for latest update

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -14,12 +14,20 @@ export class GalleryComponent implements OnInit {
 
   ngOnInit() {
     this.images = [];
-    this.images.push({source: 'assets/gallery/gallery-1.jpg', alt: '', title: 'Sample #1'});
-    this.images.push({source: 'assets/gallery/gallery-2.jpg', alt: '', title: 'Sample #2'});
-    this.images.push({source: 'assets/gallery/gallery-3.jpg', alt: '', title: 'Sample #3'});
-    this.images.push({source: 'assets/gallery/gallery-4.jpg', alt: '', title: 'Sample #4'});
-    this.images.push({source: 'assets/gallery/gallery-5.jpg', alt: '', title: 'Sample #5'});
-    this.images.push({source: 'assets/gallery/gallery-6.jpg', alt: '', title: 'Sample #6'});
-    this.images.push({source: 'assets/gallery/gallery-7.jpg', alt: '', title: 'Sample #7'});
+    // this.images.push({source: 'assets/gallery/gallery-1.jpg', alt: '', title: 'Sample #1'});
+    // this.images.push({source: 'assets/gallery/gallery-2.jpg', alt: '', title: 'Sample #2'});
+    // this.images.push({source: 'assets/gallery/gallery-3.jpg', alt: '', title: 'Sample #3'});
+    // this.images.push({source: 'assets/gallery/gallery-4.jpg', alt: '', title: 'Sample #4'});
+    // this.images.push({source: 'assets/gallery/gallery-5.jpg', alt: '', title: 'Sample #5'});
+    // this.images.push({source: 'assets/gallery/gallery-6.jpg', alt: '', title: 'Sample #6'});
+    // this.images.push({source: 'assets/gallery/gallery-7.jpg', alt: '', title: 'Sample #7'});
+    this.images.push({source: 'assets/gallery/gallery2-1.jpg', alt: '', title: 'Sample #1'});
+    this.images.push({source: 'assets/gallery/gallery2-2.jpg', alt: '', title: 'Sample #2'});
+    this.images.push({source: 'assets/gallery/gallery2-3.jpg', alt: '', title: 'Sample #3'});
+    this.images.push({source: 'assets/gallery/gallery2-4.jpg', alt: '', title: 'Sample #4'});
+    this.images.push({source: 'assets/gallery/gallery2-5.jpg', alt: '', title: 'Sample #5'});
+    this.images.push({source: 'assets/gallery/gallery2-6.jpg', alt: '', title: 'Sample #6'});
+    this.images.push({source: 'assets/gallery/gallery2-7.jpg', alt: '', title: 'Sample #7'});
+    this.images.push({source: 'assets/gallery/gallery2-8.jpg', alt: '', title: 'Sample #8'});
   }
 }
